0ed52e7fc192f8e4b100852f15a054e2131eadf8
Jacob Schrott <0062@htl.rennweg.at>
2025-04-08 00:26:01 +0200

    Update composer.json to include 'georgringer/news' dependency and remove it from ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,7 +9,6 @@ $EM_CONF[$_EXTKEY] = [
             'typo3' => '13.4.0-13.4.99',
             'fluid_styled_content' => '13.4.0-13.4.99',
             'rte_ckeditor' => '13.4.0-13.4.99',
-            'news' => '12.3.0-12.3.99',
         ],
         'conflicts' => [
         ],
