04d1f3597e6f7e2fd71519b34ffd5715966be59d
Jacob Schrott <0062@htl.rennweg.at>
2025-04-08 08:22:16 +0200

    Updated SQL insert statements to reflect correct parent IDs for 'TrustedNewsReporters' and 'Bob Bernsteiner', ensuring proper group associations.

--- ext_tables_static+adt.sql
+++ ext_tables_static+adt.sql
@@ -4,12 +4,12 @@ DELETE FROM be_groups WHERE uid = 10;
 
 -- FE-Gruppe: TrustedNewsReporters
 INSERT INTO fe_groups (uid, pid, title, hidden) VALUES
-(1, 1, 'TrustedNewsReporters', 0);
+(1, 4, 'TrustedNewsReporters', 0);
 
 -- BE-Gruppe: News publication
 INSERT INTO be_groups (uid, pid, title, hidden) VALUES
-(10, 0, 'News publication', 0);
+(10, 1, 'News publication', 0);
 
 -- FE-Benutzer: Bob Bernsteiner
 INSERT INTO fe_users (pid, username, password, usergroup, disable) VALUES
-(1, 'bob.bernsteiner', '$2y$10$QeFG1F5sIa7HpB9Q33jxI.hvN7DVGCPUnZvqJZKslOL9Q5bU4I57O', 1, 0);
+(4, 'bob.bernsteiner', '$2y$10$QeFG1F5sIa7HpB9Q33jxI.hvN7DVGCPUnZvqJZKslOL9Q5bU4I57O', 1, 0);
